ebda1ddc1fada6a6bc46af3c0707e4f70783bfa9
Shubhransh Gupta <gupta.subhransh@gmail.com>
2020-09-09 14:01:41 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
 	$pass = "";
 	$database = "economics";
 
-	$conn = new mysqli($host, $user, $pass, $database);
+	$conn = mysqli_connect($host, $user, $pass, $database);
 
 	if ($conn->connect_error) {
     	die("Connection failed: " . $conn->connect_error);
